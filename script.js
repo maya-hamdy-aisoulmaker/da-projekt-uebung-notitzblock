df3c97d11583b6e02ccfc6561937637e32744fed
Maya Hamdy <info@ki-konzeptagentur.de>
2025-08-19 06:39:14 +0200

    Entwicklung Notizblock als Vorläufer für den Warenkorb

---
+++ script.js
@@ -0,0 +1,34 @@
+// Ich erstelle einen Notizblock. Was muss der Notizblock können?
+
+
+/* Notizen erfassen: 
+- Fenster/Container zum Erfassen der Notizen -  Notizen brauchen einen closeBtn || saveBtn
+- Platzhalter Notizentext  in HTML <p id="Notizentext">Notizentext</p>
+*/
+
+
+/* Notizen speichern: 
+- Speicherort
+- Datum- und Uhrzeitangabe mitspeichern
+- Speicherbutton
+*/
+
+
+/* Notizen bei Aufruf anzeigen:
+- Fenster mit gespeicherter Notiz anzeigen lassen -> setzt Zugriff auf Speicherort voraus
+*/
+
+
+// Notizen löschen 
+
+
+
+/* Neues Eingabefenster für weitere Notizen
+- leeres Fenster, um neue Notiz zu schreiben -> Fenster muss geleert werden bei Aufruf
+*/
+
+
+
+// Suchfunktion für bereits erfasste Notizen
+// Archivierungsmöglichkeit 
+// Notizen öffnen und schließen können
